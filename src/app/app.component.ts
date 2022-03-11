afcc37dd0d6207d41c0581aeff8b3bff3639cc60
vishnu-3001 <vishnusai3001@yahoo.com>
2022-03-11 10:57:08 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit} from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Detail } from './detail.model';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'genderdetect';
+  constructor(private http:HttpClient){}
+  pred:any=[];
+  detail:{"countryid":string,"probability":number}
+  path:string;
+  nameform:FormGroup;
+  ngOnInit(){
+      this.nameform=new FormGroup({
+        namevalue:new FormControl()
+      })
+  }
+  onsubmit(){
+    console.log(this.nameform.value["namevalue"]);
+    this.path="https://api.nationalize.io/?name=";
+    this.path+=this.nameform.value["namevalue"];
+    console.log(this.path);
+     this.http.get(this.path).subscribe((data)=>{
+        for(let i=0;i<3;i++){
+          i.toString();
+          this.pred.push(new Detail(data["country"][i]["country_id"],data["country"][i]["probability"]));
+        }
+        console.log(this.pred);
+      })
+  }
 }
---
+++ src/app/detail.model.ts
@@ -0,0 +1,8 @@
+export class Detail{
+    public countryid:string;
+    public prob:number;
+    constructor(countryid:string,prob:number){
+        this.prob=prob;
+        this.countryid=countryid;
+    }
+}
